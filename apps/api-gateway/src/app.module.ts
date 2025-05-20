619a1570106a89a63a7243bb70da2987fca6b44e
volodymyr-miretskyi <vova.miretskyi@gmail.com>
2025-05-20 23:43:07 +0300

    api-gateway: fixed .env issues

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -4,11 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ImageManagerModule } from './modules/image-manager/image-manager.module';
 
 @Module({
-  imports: [
-    ImageManagerModule,
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-  ],
+  imports: [ImageManagerModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
--- apps/api-gateway/src/modules/image-manager/image-manager.module.ts
+++ apps/api-gateway/src/modules/image-manager/image-manager.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { ImageManagerService } from './image-manager.service';
 import { ImageManagerController } from './image-manager.controller';
@@ -7,17 +8,22 @@ import { DEFAULT_RABBIT_MQ_URL, QUEUES, SERVICES } from '@/constants';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: SERVICES.IMAGE_SERVICE,
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBIT_MQ_URL || DEFAULT_RABBIT_MQ_URL],
-          queue: QUEUES.IMAGE_SERVICE,
-          queueOptions: {
-            durable: false,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              configService.get<string>('RABBIT_MQ_URL') ??
+                DEFAULT_RABBIT_MQ_URL,
+            ],
+            queue: QUEUES.IMAGE_SERVICE,
+            queueOptions: { durable: false },
           },
-        },
+        }),
       },
     ]),
   ],
