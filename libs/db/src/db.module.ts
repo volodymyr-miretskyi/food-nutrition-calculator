9e4f7e4571a240c240559f93a4d494ded3de77da
volodymyr-miretskyi <vova.miretskyi@gmail.com>
2025-05-20 23:42:23 +0300

    db: created db module

---
+++ libs/db/src/db.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DBModule {}
