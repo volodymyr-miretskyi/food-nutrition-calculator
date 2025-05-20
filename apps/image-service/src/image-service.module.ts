9c4934b5269e10542e68c33eed757bbc604cfbb4
volodymyr-miretskyi <vova.miretskyi@gmail.com>
2025-05-20 23:43:39 +0300

    image-service: implemented photo data db saving

--- apps/image-service/src/image-service.module.ts
+++ apps/image-service/src/image-service.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { S3Module } from '@/libs/aws/s3/src/s3.module';
+import { S3Module } from '@libs/aws/s3/src/s3.module';
+import { Photo, PhotoSchema } from '@schemas/photo.schema';
 
 import { ImageServiceService } from './image-service.service';
 import { ImageServiceController } from './image-service.controller';
+import { DBModule } from '@db/src/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), S3Module],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: Photo.name, schema: PhotoSchema }]),
+    DBModule,
+    S3Module,
+  ],
   controllers: [ImageServiceController],
   providers: [ImageServiceService],
 })
--- apps/image-service/src/image-service.service.ts
+++ apps/image-service/src/image-service.service.ts
@@ -1,16 +1,30 @@
-import { S3Service } from '@/libs/aws/s3/src/s3.service';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { S3Service } from '@libs/aws/s3/src/s3.service';
+import { Photo } from '@db/schemas/photo.schema';
+import { SavePhotoDto } from '@dto/photo.dto';
 
 @Injectable()
 export class ImageServiceService {
-  constructor(private readonly s3: S3Service) {}
+  constructor(
+    @InjectModel(Photo.name) private photoModal: Model<Photo>,
+    private readonly s3: S3Service,
+  ) {}
 
   getHello(): string {
     return 'Hello World!';
   }
 
   async uploadImage(file: Express.Multer.File) {
-    const result = await this.s3.upload(file);
-    return result;
+    const s3result = await this.s3.upload(file);
+    const params: SavePhotoDto = {
+      ...s3result,
+    };
+
+    const photo = await this.photoModal.create(params);
+
+    return photo;
   }
 }
